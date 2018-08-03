f8663b9ae6520aa5a33dc243d33f189bf0c6ad4f
Alexander Hartson <alexander-hartson@live.com>
2018-08-02 19:35:09 -0500

    adding database beans

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,8 +2,10 @@ package com.packt.cardatabase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -14,5 +16,12 @@ public class CardatabaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
         logger.info("Hello Spring Boot");
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+          // Save demo data to database
+        };
     }
 }
