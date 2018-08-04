ec62585f3cf5ddd71c11266b6ed705aa39e9b374
Alexander Hartson <alexander-hartson@live.com>
2018-08-03 21:56:22 -0500

    adding params

--- src/main/java/com/packt/cardatabase/domain/CarReposity.java
+++ src/main/java/com/packt/cardatabase/domain/CarReposity.java
@@ -3,15 +3,15 @@ package com.packt.cardatabase.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CarReposity extends CrudRepository <Car,Long> {
 
     // Fetch cars by brand
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
 
     // Fetch cars by color
-    List<Car> findByColor(String color);
-
-    // Fetch cars by year
-    List<Car> findByYear(int year);
+    List<Car> findByColor(@Param("color") String color);
 }
