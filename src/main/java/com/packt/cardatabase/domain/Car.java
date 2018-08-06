6c0b81029315e89f7e96b5be594c54032ef40f40
Alexander Hartson <alexander-hartson@live.com>
2018-08-06 14:19:57 -0500

    adding REST test case

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -20,8 +20,6 @@ public class Car {
 	@JoinColumn(name = "owner")
 	private Owner owner;
 	
-	public Car() {}
-	
 	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
@@ -32,7 +30,15 @@ public class Car {
 		this.price = price;
 		this.owner = owner;
 	}
-	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public Owner getOwner() {
 		return owner;
 	}
@@ -44,39 +50,48 @@ public class Car {
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String model) {
 		this.model = model;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getRegisterNumber() {
 		return registerNumber;
 	}
+
 	public void setRegisterNumber(String registerNumber) {
 		this.registerNumber = registerNumber;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	
 }
