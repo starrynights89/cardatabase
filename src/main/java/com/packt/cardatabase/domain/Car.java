246d222830d32b43ca4a3416b135b2aab465d6e3
Alexander Hartson <alexander-hartson@live.com>
2018-08-12 13:43:22 -0500

    changes to Car and CarRepositoryTest

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -19,7 +19,19 @@ public class Car {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "owner")
 	private Owner owner;
-	
+
+	public Car() {}
+
+	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+	}
+
 	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
