2ebe255fbbf7f367398591094ac9e845e58d7214
Alexander Hartson <alexander-hartson@live.com>
2018-08-17 03:23:17 -0500

    adding fixes to authorization

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Add this row to allow access to all endpoints
-        http.cors().and().authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
         /* Comment this out
         http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login")
